84015005992db61fa262afdaa7c125090c7e819b
bentleyminwa <bentleyminwa@gmail.com>
2025-02-09 13:38:27 +0300

    Added search icon to SearchComponent; enhance visual appeal and user interaction

--- src/shared/components/SearchComponent/SearchComponent.tsx
+++ src/shared/components/SearchComponent/SearchComponent.tsx
@@ -1,3 +1,5 @@
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
 const SearchComponent = () => {
   return (
     <form className="w-full flex justify-center items-center">
@@ -6,6 +8,8 @@ const SearchComponent = () => {
         placeholder="search products"
         className="border border-gray-200 my-5 p-3 w-1/2 md:w-1/3 rounded-sm focus:outline-none text-gray-500 tracking-wider"
       />
+
+      <FaMagnifyingGlass className="text-xl -ml-8 bg-white" />
     </form>
   );
 };
