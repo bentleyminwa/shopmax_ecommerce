760d8eb6de05f8bdf94365e8bee4cf963afe93df
bentleyminwa <bentleyminwa@gmail.com>
2025-02-09 08:05:56 +0300

    Added cart item count to Navbar; refactor NavIcons layout for improved structure

--- src/shared/components/Navbar/NavIcons.tsx
+++ src/shared/components/Navbar/NavIcons.tsx
@@ -1,21 +1,33 @@
 import { NavLink } from "react-router-dom";
 import { appData } from "../../../assets/data/data";
+import { useCart } from "../../hooks/useCart";
 import { HamburgerComponent } from "./index";
 
 const NavIcons = () => {
   const { menuIcons } = appData;
+  const { cart } = useCart();
 
   return (
-    <ul className="flex justify-center items-center gap-4 text-gray-700">
-      {menuIcons.map((menuIcon) => (
-        <li key={menuIcon.name} className="text-2xl">
-          <NavLink to={menuIcon.path}>{menuIcon.icon}</NavLink>
-        </li>
-      ))}
-      <li>
-        <HamburgerComponent />
-      </li>
-    </ul>
+    <div className="flex justify-center items-center gap-4">
+      <ul className="flex justify-center items-center gap-4 text-gray-700">
+        {menuIcons.map((menuIcon, index) => (
+          <li
+            key={menuIcon.name}
+            className={`text-2xl ${index === 1 ? "relative" : ""}`}
+          >
+            <NavLink to={menuIcon.path}>{menuIcon.icon}</NavLink>
+            {index === 1 ? (
+              <span className="absolute -top-4 -right-3 bg-red-500 text-white w-6 h-6 flex justify-center items-center text-xs rounded-full font-bold">
+                {cart.length}
+              </span>
+            ) : (
+              ""
+            )}
+          </li>
+        ))}
+      </ul>
+      <HamburgerComponent />
+    </div>
   );
 };
 
