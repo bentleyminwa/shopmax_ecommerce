7a0a36c35458d751d379447d6729cacaeb8ecf1a
bentleyminwa <bentleyminwa@gmail.com>
2025-02-09 08:17:03 +0300

    Enhanced Navbar MenuItems with active link styling for improved user experience

--- src/shared/components/Navbar/MenuItems.tsx
+++ src/shared/components/Navbar/MenuItems.tsx
@@ -8,7 +8,14 @@ const MenuItems = () => {
     <ul className="hidden md:flex gap-5 lg:gap-10 text-xs text-jet font-semibold">
       {menuItems.map((item) => (
         <li key={item.id}>
-          <NavLink to={item.path} className="tracking-widest">
+          <NavLink
+            to={item.path}
+            className={({ isActive }) =>
+              `tracking-widest pb-1 hover:border-b-2 ${
+                isActive ? "border-b-2" : ""
+              }`
+            }
+          >
             {item.name}
           </NavLink>
         </li>
