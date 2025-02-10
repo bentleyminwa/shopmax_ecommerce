34407d1f5a6bcedaca736c8344ecfa87ab7ae10c
bentleyminwa <bentleyminwa@gmail.com>
2025-02-10 07:10:14 +0300

    Enhanced Button component with hover effect for improved user interaction

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 const Button: FC<ButtonProps> = ({ children, ...rest }) => {
   const btnClasses = className(
     rest.className,
-    "bg-black text-white py-2 px-3 uppercase text-xs tracking-wide cursor-pointer w-fit"
+    "bg-black text-white py-2 px-3 uppercase text-xs tracking-wide cursor-pointer w-fit hover:scale-105 transition duration-300 ease-in-out"
   );
 
   return (
